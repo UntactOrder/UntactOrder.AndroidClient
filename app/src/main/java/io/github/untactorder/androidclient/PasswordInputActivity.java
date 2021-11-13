ff4d239c5a3920fbb73f1cae18dca5edd892be79
IRACK <81044362+IRACK000@users.noreply.github.com>
2021-11-13 18:37:57 +0900

    PasswordInputActivity 추가

---
+++ app/src/main/java/io/github/untactorder/androidclient/PasswordInputActivity.java
@@ -0,0 +1,177 @@
+package io.github.untactorder.androidclient;
+
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.core.content.ContextCompat;
+
+enum InputType {SignUp, Confirm, SignIn, Retry}
+
+public class PasswordInputActivity extends AppCompatActivity {
+    public static final int RESULT_INCORRECT = -300;
+
+    boolean __DEBUG = true;
+
+    protected void println(String tag, String data) {
+        if (__DEBUG) {
+            Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+        }
+        Log.d(tag, data);
+    }
+    protected void println(String data) {
+        println("PwIn", data);
+    }
+
+    InputType inputType;
+    protected int repeatCount;
+    protected String intentPassword;
+    TextView[] circleView = new TextView[6];
+    protected String password = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pwin);
+
+        Intent intent;
+        if (__DEBUG) {
+            intent = new Intent();
+            intent.putExtra("table_name", "창가 7번");
+            intent.putExtra("input_type", InputType.SignIn);
+            println("pwin", "디버그 모드");
+        } else {
+            intent = getIntent();
+            println("pwin", "릴리즈 모드");
+        }
+
+        TextView tableName = findViewById(R.id.pwin_tv_table_num);
+        tableName.setText(intent.getStringExtra("table_name"));
+        println("테이블 이름 설정 완료");
+        TextView guide = findViewById(R.id.pwin_tv_guide_msg);
+        inputType = (InputType) intent.getSerializableExtra("input_type");
+        switch (inputType) {
+            case SignUp:
+                guide.setText(R.string.at_pwin_guide_new);
+                println("입력 타입 설정 : SignUp");
+                break;
+            case Confirm:
+                guide.setText(R.string.at_pwin_guide_confirm);
+                repeatCount = intent.getIntExtra("repeat_count", 0);
+                intentPassword = intent.getStringExtra("signup_password");
+                println("입력 타입 설정 : Confirm");
+                break;
+            case SignIn:
+                guide.setText(R.string.at_pwin_guide);
+                println("입력 타입 설정 : SignIn");
+                break;
+            case Retry:
+                guide.setText(R.string.at_pwin_guide_retry);
+                guide.setTextColor(ContextCompat.getColor(this, R.color.magenta));
+                println("입력 타입 설정 : Retry");
+                break;
+        }
+
+        int[] ids = new int[]{R.id.pwin_tv_pw_1st, R.id.pwin_tv_pw_2nd, R.id.pwin_tv_pw_3rd,
+                R.id.pwin_tv_pw_4th, R.id.pwin_tv_pw_5th, R.id.pwin_tv_pw_6th};
+        for (int i = 0; i < ids.length; i++) {
+            circleView[i] = findViewById(ids[i]);
+        }
+    }
+
+    public void onClearAllClicked(View v) {
+        println("Clear all");
+        for (int i = 0; i < circleView.length; i++) {
+            setCircleView(i, null, false);
+        }
+        password = "";
+    }
+
+    public void onClearOneClicked(View v){
+        println("clear");
+        int length = password.length();
+        if (length >= 2) {
+            setCircleView(length-1, null, false);
+            setCircleView(length-2, "", false);
+            password = password.substring(0, length-1);
+        } else if (length == 1) {
+            setCircleView(0, null, false);
+            password = "";
+        }
+    }
+
+    protected void showNumPadInput(int number) {
+        int length = password.length();
+        if (length >= 1) {
+            setCircleView(length, ""+number, true);
+            setCircleView(length-1, "", false);
+        } else {
+            setCircleView(0, ""+number, true);
+        }
+        password += number;
+    }
+
+    protected void setCircleView(int th, String text, boolean transparent) {
+        /* text가 null이라는 것은 비밀번호가 그 자리에 아예 없다는 것이고
+         * text가 빈 스트링이라는 것은 비밀번호를 보여주지 않는다는 것으로 해석한다.
+         */
+        TextView target = circleView[th];
+        if (text == null) {
+            target.setText("");
+            if (transparent) {
+                target.setBackgroundTintList(ColorStateList.valueOf(
+                        ContextCompat.getColor(this, R.color.at_pwin_pw_view_circle_pressed)));
+            } else {
+                target.setBackgroundTintList(ColorStateList.valueOf(
+                        ContextCompat.getColor(this, R.color.at_pwin_pw_view_circle_ordinal)));
+            }
+        } else {
+            target.setText(text);
+            if (transparent) {
+                target.setBackgroundTintList(ColorStateList.valueOf(
+                        ContextCompat.getColor(this, R.color.at_pwin_pw_view_circle_pressed)));
+            } else {
+                target.setBackgroundTintList(ColorStateList.valueOf(
+                        ContextCompat.getColor(this, R.color.at_pwin_pw_view_circle_filled)));
+            }
+        }
+    }
+
+    public void onNumPadClicked(View v) {
+        println("numpad clicked");
+        int clicked = Integer.parseInt((String) ((TextView) v).getText());
+        password += clicked;
+        showNumPadInput(clicked);
+
+        if (password.length() >= 6) {
+            finish();
+        }
+    }
+
+    protected void showDialog(String msg) {
+        println("show dialog");
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage(msg);
+        alertDialogBuilder.setPositiveButton(R.string.text_okay, (dialog, which) -> {});
+        alertDialogBuilder.create().show();
+    }
+
+    @Override
+    public void finish() {
+        Intent result = getIntent();
+        if (password.length() < 6) {
+            setResult(RESULT_CANCELED, result);
+        } else if (password.equals(intentPassword)) {
+            String msg = getResources().getString(R.string.at_pwin_wrong_password_msg)+'('+repeatCount+"/3)";
+            showDialog(msg);
+            setResult(RESULT_INCORRECT, result);
+        } else {
+            setResult(RESULT_OK, result);
+        }
+    }
+}
