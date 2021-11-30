225a7d4f1ad59f794ddb6e5e2dae6c8cfb7d5235
IRACK <81044362+IRACK000@users.noreply.github.com>
2021-11-30 18:36:44 +0900

    .

--- app/src/main/java/io/github/untactorder/androidclient/PasswordInputActivity.java
+++ app/src/main/java/io/github/untactorder/androidclient/PasswordInputActivity.java
@@ -19,7 +19,7 @@ public class PasswordInputActivity extends AppCompatActivity {
     public static final int RESULT_INCORRECT = -300;
 
     String TAG = "PwIn";
-    boolean __DEBUG = true;
+    boolean __DEBUG = false;
 
     protected void println(String tag, String data, boolean showToast) {
         if (showToast) {
