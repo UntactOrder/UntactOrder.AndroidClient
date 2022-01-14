178e1f2c33094f93464e880ab08f2ed48a8b8d66
IRACK <81044362+IRACK000@users.noreply.github.com>
2022-01-14 16:58:24 +0900

    구조 변경 시도

---
+++ app/src/orderAssistant/kotlin/io/github/untactorder/GlobalApplication.kt
@@ -0,0 +1,14 @@
+package io.github.untactorder
+
+import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
+//import io.github.untactorder.orderassistant.BuildConfig
+
+class GlobalApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // 다른 초기화 코드들
+
+
+    }
+}
